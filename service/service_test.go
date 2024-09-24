32b591afaef6edcf2c73da97bedc55db1aa80a8a
Mahmoud ElKodary <melkodary@outlook.com>
2024-09-24 18:35:49 +0200

    testing smart contract interactions

--- service/service_test.go
+++ service/service_test.go
@@ -1,46 +1,147 @@
 package service
 
-import "testing"
+import (
+	"math/big"
+	"testing"
 
-type MockEvmosClient struct{}
+	"github.com/stretchr/testify/assert"
+)
 
-func (*MockEvmosClient) GetAccounts() ([]string, error) {
-	return []string{}, nil
+type MockEvmosClient struct {
+	accounts         []string
+	block            map[string]interface{}
+	blockNumber      string
+	transactionTrace map[string]interface{}
+	code             map[string]string
+	blocksInRange    []map[string]interface{}
+	balance          string
 }
 
-// TODO - create a random block
-func (*MockEvmosClient) GetBlock(blockNumber string) (map[string]interface{}, error) {
-	return map[string]interface{}{}, nil
+func (m *MockEvmosClient) GetAccounts() ([]string, error) {
+	return m.accounts, nil
+}
+
+func (m *MockEvmosClient) GetBlock(blockNumber string) (map[string]interface{}, error) {
+	return m.block, nil
 }
 
 func (m *MockEvmosClient) GetBlockNumber() (string, error) {
-	return "0x1", nil
+	return m.blockNumber, nil
 }
 
-// TODO: create a random transaction
 func (m *MockEvmosClient) GetTransactionTrace(txHash string) (map[string]interface{}, error) {
-	return map[string]interface{}{"calls": []interface{}{}}, nil
+	return m.transactionTrace, nil
 }
 
 func (m *MockEvmosClient) GetCode(address, blockNumber string) (string, error) {
-	return "0x", nil
+	if code, exists := m.code[address]; exists {
+		return code, nil
+	}
+	return "", nil
 }
 
-func (m *MockEvmosClient) GetBlocksInRange(start, end int) ([]map[string]interface{}, error) {
-	return []map[string]interface{}{}, nil
+func (m *MockEvmosClient) GetBlocksInRange(startBlock, endBlock int) ([]map[string]interface{}, error) {
+	return m.blocksInRange, nil
 }
 
 func (m *MockEvmosClient) GetBalance(address, block string) (string, error) {
-	return "0x1", nil
+	return m.balance, nil
 }
 
 func TestGetLatestBlock(t *testing.T) {
-	SetClient(&MockEvmosClient{})
-	block, err := GetLatestBlock()
-	if err != nil {
-		t.Errorf("Expected no error, got %v", err)
+	client := &MockEvmosClient{
+		blockNumber: "0x1",
 	}
-	if block != "0x1" {
-		t.Errorf("Expected block number 0x1, got %v", block)
+	SetClient(client)
+	block, err := GetLatestBlock()
+	assert.NoError(t, err)
+	assert.Equal(t, "0x1", block)
+}
+
+func TestGetSmartContracts(t *testing.T) {
+	client := &MockEvmosClient{
+		blocksInRange: []map[string]interface{}{
+			{
+				"transactions": []interface{}{
+					map[string]interface{}{
+						"hash":            "0xTxHash1",
+						"to":              "0xContractAddress1",
+						"contractAddress": nil,
+					},
+					map[string]interface{}{
+						"hash":            "0xTxHash2",
+						"to":              nil,
+						"contractAddress": "0xContractAddress2",
+					},
+					map[string]interface{}{
+						"hash":            "0xTxHash3",
+						"to":              "0xContractAddress3",
+						"contractAddress": nil,
+					},
+					map[string]interface{}{
+						"hash":            "0xTxHash4",
+						"to":              "0xContractAddress4",
+						"contractAddress": nil,
+					},
+					map[string]interface{}{
+						"hash":            "0xTxHash5",
+						"to":              "0xContractAddress1",
+						"contractAddress": nil,
+					},
+					map[string]interface{}{
+						"hash":            "0xTxHash6",
+						"to":              "0xContractAddress3",
+						"contractAddress": nil,
+					},
+					map[string]interface{}{
+						"hash":            "0xTxHash7",
+						"to":              "0xContractAddress4",
+						"contractAddress": nil,
+					},
+					map[string]interface{}{
+						"hash":            "0xTxHash8",
+						"to":              "0xContractAddress4",
+						"contractAddress": nil,
+					},
+				},
+			},
+		},
+		transactionTrace: map[string]interface{}{
+			"calls": []interface{}{
+				map[string]interface{}{
+					"to": "0xContractAddress2",
+				},
+			},
+		},
+		code: map[string]string{
+			"0xContractAddress1": "0x6001600101",
+			"0xContractAddress2": "0x6001600102",
+			"0xContractAddress3": "0x6001600103",
+			"0xContractAddress4": "0x6001600104",
+		},
+	}
+	SetClient(client)
+
+	expectedContracts := []kv{
+		{"0xContractAddress2", big.NewInt(9)},
+		{"0xContractAddress4", big.NewInt(3)},
+		{"0xContractAddress3", big.NewInt(2)},
+		{"0xContractAddress1", big.NewInt(2)},
+	}
+
+	contracts, err := GetSmartContracts(100, 200)
+
+	assert.NoError(t, err)
+	assert.Equal(t, len(expectedContracts), len(contracts))
+
+	for i, contract := range contracts {
+		assert.Equal(t, expectedContracts[i].Key, contract.Key)
+		if expectedContracts[i].Value.Cmp(contract.Value) != 0 {
+			t.Errorf("Value mismatch for contract %s: expected %s, got %s", contract.Key, expectedContracts[i].Value.String(), contract.Value.String())
+		}
+	}
+
+	for i, expectedContract := range expectedContracts {
+		assert.Equal(t, expectedContract.Key, contracts[i].Key)
 	}
 }
